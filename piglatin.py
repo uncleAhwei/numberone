243cdc6ac3832339441326f13592ed07a56e6681
uncleAhwei <nikolas.weiss@gmail.com>
2014-07-11 11:17:03 +0800

    Fixed another error? Grr! This version should work

--- piglatin.py
+++ piglatin.py
@@ -1,9 +1,9 @@
 pyg = "ay"
-original = raw_input("Enter a word to translate it into pig latin:\n")
+original = raw_input("Enter a word to translate it into pig latin:")
 if len(original) > 0 and original.isalpha():
-	word = original.lower()
-	first = word[0]
-	new_word = word[1:len(new_word)] + first + pyg
-	print new_word
+    word = original.lower()
+    first = word[0]
+    new_word = word[1:] + first + pyg
+    print new_word
 else:
-	print "You didn't type a word, dummy! Words only have letters, no numbers or spaces, please."
+    print "You didn't type a word, dummy! Words only have letters, no numbers or spaces, please."
