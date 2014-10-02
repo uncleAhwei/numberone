888b5d7d613d4d1670cff1e9cbbf925b5b66f7c3
uncleAhwei <nikolas.weiss@gmail.com>
2014-10-02 23:10:53 +0800

    Modified the pig latin translator so that it will continually prompt the user for words until told to quit.

--- piglatin.py
+++ piglatin.py
@@ -1,10 +1,22 @@
-pyg = "ay"
-original = raw_input("Enter a word to translate it into pig latin:")
-if len(original) > 0 and original.isalpha():
-    word = original.lower()
-    first = word[0]
-    new_word = word[1:] + first + pyg
-    print new_word
+#! python2
+import time
+
+pyg = 'ay'
+
+
+
+while True:
+    original = raw_input('Enter a word:')
+    if len(original) > 0 and original.isalpha():
+        word = original.lower()
+        first = word[0]
+        new_word = word[1:] + first + pyg
+        print new_word
     continue
-else:
-    print "You didn't type a word, dummy! Words only have letters, no numbers or spaces, please."
+
+
+    if original.lower() == "quit" or original.lower() == "done": break
+
+
+
+
